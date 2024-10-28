188c547ef8ac625a8e94895a0d6033fef0d6d88a
Guilherme Cardoso <gcs200206@gmail.com>
2024-10-28 01:17:08 -0400

    Colocando responsividade na seção de localização

--- src/components/Localizacao/index.tsx
+++ src/components/Localizacao/index.tsx
@@ -1,16 +1,18 @@
 import Image from "next/image";
-import Mapa from "./assets/mapa.png"
+import Mapa from "./assets/mapa.png";
 import Link from "next/link";
 
 export default function Localizacao() {
     return (
-        <section className="mt-16 pt-12 bg-white flex pb-10 items-center text-center">
-            <div>
+        <section className="mt-16 p-12 bg-white flex flex-col lg:flex-row pb-10 items-center text-center lg:space-x-10">
+            <div className="lg:w-1/2">
                 <h1 className="font-bold text-4xl">Encontre Oficinas Próximas</h1>
-                <p className="mt-5 mb-10 w-3/4 m-auto">Estamos aqui para ajudar você a resolver os problemas do seu carro! Utilize nosso recurso de localização para encontrar oficinas confiáveis nas proximidades. Clique no botão abaixo e descubra onde você pode levar seu veículo.</p>
-                <Link href="/oficinas" className="m-auto mb-20 bg-blue-500 rounded p-3 text-white hover:bg-blue-600 transition">Ver Oficinas</Link>
+                <p className="py-10 m-auto text-xl w-full lg:w-3/4">Estamos aqui para ajudar você a resolver os problemas do seu carro! Utilize nosso recurso de localização para encontrar oficinas confiáveis nas proximidades. Clique no botão abaixo e descubra onde você pode levar seu veículo.</p>
+                <Link href="/oficinas" className=" bg-blue-500 rounded p-3 text-white hover:bg-blue-600 transition lg:mb-10">Ver Oficinas</Link>
             </div>
-            <Image src={Mapa} alt="Mapa de Localização" className="mr-10"></Image>
+            <div className="lg:w-1/2">
+                <Image src={Mapa} alt="Mapa de Localização" className="my-10 w-full h-auto" />
+            </div>
         </section>
-    )
+    );
 }
